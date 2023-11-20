419e62120f29dbe817b397adaff4aa21f6f2a574
Hernán Castañón <herna@paranoidandroid.co>
2023-11-20 16:43:07 +0100

    phone2: remove unexistent repo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,8 +86,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.nothing
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(DEVICE_PATH)/device_framework_matrix.xml \
-    vendor/nxp/nfc/vendor_framework_compatibility_matrix.xml
+    $(DEVICE_PATH)/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 
